a1fada1fef8b858aab3a0f8db1b454fe9974d2f1
Vince Brown <vince.brown@hingehealth.com>
2020-08-04 14:23:21 -0700

    docs: updating package jsons

--- packages/Button/stories/Button.stories.js
+++ packages/Button/stories/Button.stories.js
@@ -2,6 +2,6 @@ import React from "react"
 
 import { storiesOf } from "@storybook/react-native"
 
-import Button from "./Button"
+import Button from "../lib/Button"
 
 storiesOf("Button", module).add("with text", () => <Button />)
